a48b08df480ada9d30da56b789c92436b5ba1286
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-05-03 16:28:13 +0530

    Added String utils

---
+++ src/main/java/github/com/yadavsudhir405/api/StringUtil.java
@@ -0,0 +1,12 @@
+package github.com.yadavsudhir405.api;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtil {
+    public String toUpperCase(String str){
+        return StringUtils.upperCase(str);
+    }
+    public String toLowerCase(String str){
+        return StringUtils.lowerCase(str);
+    }
+}
