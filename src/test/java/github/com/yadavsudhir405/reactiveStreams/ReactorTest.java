68904ee68745cd90f893ac97d5f0ae8591ee88c5
Sudhir Yadav <yadavsudhir405@gmail.com>
2021-04-22 16:58:40 +0530

    merge two streams

--- src/test/java/github/com/yadavsudhir405/reactiveStreams/ReactorTest.java
+++ src/test/java/github/com/yadavsudhir405/reactiveStreams/ReactorTest.java
@@ -1,11 +1,19 @@
 package github.com.yadavsudhir405.reactiveStreams;
 
+import org.junit.After;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReactorTest {
+    private static AtomicInteger count = new AtomicInteger(3);
+
     @Test
     public void test1() {
         Flux.just("Hello", "World")
@@ -21,5 +29,34 @@ public class ReactorTest {
                 .sort()
                 .zipWith(Flux.range(1, 100), (word, line) -> line +". "+ word)
                 .subscribe(System.out::println);
+    }
+
+    @Test
+    public void test3() {
+        Flux<String> fastClock = Flux.interval(Duration.ofSeconds(1)).map(tick -> "fast" + tick);
+        Flux<String> slowClock = Flux.interval(Duration.ofSeconds(2)).map(tick -> "slow" + tick);
+
+        Flux<String> clock = Flux.merge(
+                fastClock.filter(tick -> isFastTime()),
+                slowClock.filter(tick -> isSlowTime())
+        );
+        clock.subscribe(System.out::println);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        TimeUnit.SECONDS.sleep(30);
+    }
+
+    public static boolean isFastTime() {
+        if(count.get() > 0){
+            count.set(count.get() - 1);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isSlowTime() {
+        return count.get() <= 0;
     }
 }
