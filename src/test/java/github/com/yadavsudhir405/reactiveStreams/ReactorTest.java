7c2272e4834c6fe5f4980836b978be83ba41c4ec
Sudhir Yadav <yadavsudhir405@gmail.com>
2021-04-22 16:12:36 +0530

    just method printing whole objects of words as an item of stream

--- src/test/java/github/com/yadavsudhir405/reactiveStreams/ReactorTest.java
+++ src/test/java/github/com/yadavsudhir405/reactiveStreams/ReactorTest.java
@@ -3,10 +3,19 @@ package github.com.yadavsudhir405.reactiveStreams;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+
 public class ReactorTest {
     @Test
     public void test1() {
         Flux.just("Hello", "World")
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void test2() {
+        final List<String> words = List.of("the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
+        Flux.just(words)
                 .subscribe(System.out::println);
     }
 }
